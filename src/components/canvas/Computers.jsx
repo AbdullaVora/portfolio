db0153ad3bdf2ad6fbcb6f1be20117d0069e02ea
Abdullah <abdullahvora136@gmail.com>
2024-12-29 22:34:10 +0530

    rotate start

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -69,6 +69,8 @@ const ComputerCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          autoRotate={true}
+          autoRotateSpeed={1}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
